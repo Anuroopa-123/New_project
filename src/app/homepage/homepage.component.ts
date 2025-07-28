588caa2c684653b75145b2f5f078a4ed55edb63d
aravindh shankar <aravindhshankar@tansam.org>
2025-07-28 23:53:23 +0530

    added topbar and header

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { TopbarComponent } from "../shared/topbar/topbar.component";
 import { HeaderComponent } from "../shared/header/header.component";
+import { NavbarComponent } from "../shared/navbar/navbar.component";
 
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [TopbarComponent, HeaderComponent],
+  imports: [TopbarComponent, HeaderComponent, NavbarComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
