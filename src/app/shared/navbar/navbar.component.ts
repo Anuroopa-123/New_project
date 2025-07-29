7debe6c285a979010b1a633b6b0803736f8938f8
aravindh shankar <aravindhshankar@tansam.org>
2025-07-29 13:41:23 +0530

    Added header topbar and navbar

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,14 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
   menuOpen = false;
+router: any;
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
